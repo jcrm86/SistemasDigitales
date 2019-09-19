280ebd58e0b62405483200395fb016cadd478670
jcrm86 <jcrm86@gmail.com>
2019-09-19 18:44:03 -0500

    Implementacion de un comparador

---
+++ Sumador/Sumador/Comparador.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:50:35 09/19/2019 
+-- Design Name: 
+-- Module Name:    Comparador - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Comparador is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           C : out  STD_LOGIC);
+end Comparador;
+
+architecture Behavioral of Comparador is
+
+begin
+--Estilo funcional
+--process
+--if A=B then 
+	--C<=1;
+	--else
+	--C<=0;
+--end if;
+
+--Estilo por diagrama de flujo
+--C <= '1' when (A = B) else '0';
+
+--Estilo
+ C<= (not A and not B) or (A and B);
+
+end Behavioral;
+
---
+++ Sumador/Sumador/Sumador.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:35:49 09/19/2019 
+-- Design Name: 
+-- Module Name:    Sumador - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Sumador is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Co : in  STD_LOGIC;
+           S : out  STD_LOGIC;
+           C : out  STD_LOGIC);
+end Sumador;
+
+architecture Behavioral of Sumador is
+
+begin
+
+--Descripcion por comportamiento
+S<='0';
+C<='0';
+
+
+
+end Behavioral;
+
